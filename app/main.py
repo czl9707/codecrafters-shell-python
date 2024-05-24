d847c8248ab6b4edfb1eb930f11d86d61abbd248
Zain Chen <czl970721@gmail.com>
2024-05-24 18:32:22 -0400

    handle unknown

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 import sys
 
+def cmd_handler(cmd: str) -> None:
+    command = cmd.split(" ")[0]
+    sys.stdout.write(f"{command}: command not found")
+    sys.stdout.flush()
 
 def main():
     # Uncomment this block to pass the first stage
@@ -7,7 +11,8 @@ def main():
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    cmd = input()
+    cmd_handler(cmd)
 
 
 if __name__ == "__main__":
